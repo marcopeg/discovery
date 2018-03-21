9e647bbf37c85e643d3d7781d14079e5dcc70473
Marco Pegoraro <marco.pegoraro@gmail.com>
2018-03-21 08:35:51 +0100

    Improve time 2 string

--- src/containers/Estimate/utils/minutes.js
+++ src/containers/Estimate/utils/minutes.js
@@ -4,45 +4,91 @@ const week = wday * 5
 const wmon = wday * 20
 const wyea = wday * 250
 
+const mins2str = num => (
+    num > 1
+        ? `${num} minutes`
+        : `${num} minute`
+)
+
+const hours2str = num => (
+    num > 1
+        ? `${num} hours`
+        : `${num} hour`
+)
+
+const days2str = num => (
+    num > 1
+        ? `${num} days`
+        : `${num} day`
+)
+
+const weeks2str = num => (
+    num > 1
+        ? `${num} weeks`
+        : `${num} week`
+)
+
+const months2str = num => (
+    num > 1
+        ? `${num} months`
+        : `${num} month`
+)
+
+const years2str = num => (
+    num > 1
+        ? `${num} years`
+        : `${num} year`
+)
+
 export const minutes2string = (value) => {
     if (value >= wyea) {
         const years = Math.round((value / wyea) * 10) / 10
-        return `${years} years`
+        return `${years2str(years)}`
     }
 
     if (value >= wmon) {
         const months = Math.floor(value / wmon)
         const days = Math.floor((value - (months * wmon)) / wday)
         return days
-            ? `${months}m ${days}d`
-            : `${months}m`
+            ? `${months2str(months)}, ${days2str(days)}`
+            : `${months2str(months)}`
     }
 
     if (value >= week) {
         const weeks = Math.floor(value / week)
         const days = Math.floor((value - (weeks * week)) / wday)
         return days
-            ? `${weeks}w ${days}d`
-            : `${weeks}w`
+            ? `${weeks2str(weeks)}, ${days2str(days)}`
+            : `${weeks2str(weeks)}`
     }
 
     if (value >= wday) {
         const days = Math.floor(value / wday)
         const hours = Math.floor((value - (days * wday)) / 60)
+
+        // if (days > 0 && days < 2) {
+        //     return 'tomorrow'
+        // }
+
         return hours
-            ? `${days}d ${hours}h`
-            : `${days}d`
+            ? `${days2str(days)}, ${hours2str(hours)}`
+            : `${days2str(days)}`
     }
 
     if (value >= 60) {
         const hours = Math.floor(value / 60)
         const min = value - (hours * 60)
+
+        // if (hours > 4) {
+        //     return 'today'
+        // }
+
         return min
-            ? `${hours}h ${min}m`
-            : `${hours}h`
+            ? `${hours2str(hours)} ${mins2str(min)}`
+            : `${mins2str(min)}`
     }
 
-    return `${value} minutes`
+    return `${mins2str(value)}`
 }
 
 export const string2minutes = (str) => {
